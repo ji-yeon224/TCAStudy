43dd42b3f6f36d2e8ef3982abb8bb5205ff0dd5d
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2025-02-28 15:47:16 +0900

    Initial Commit

---
+++ TCAStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCAStudy
+//
+//  Created by Jiyeon on 2/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCAStudy/TCAStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAStudyApp.swift
+//  TCAStudy
+//
+//  Created by Jiyeon on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TCAStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
